5f9b51b284e939e2c8db138410c52c78837080b9
Your Name <you@example.com>
2025-04-01 18:28:28 +0200

    popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import InputComponent from './components/InputComponent'
 import RotateComponent from './components/RotateComponent'
+import Carousel from './components/Carousel'
+import PopUp from './components/PopUp'
 
 const App = () => {
   return (
     <div>
+      {/*
       <InputComponent />
       <RotateComponent />
+      <Carousel />
+      */}
+      
+      <PopUp />
 
     </div>
   )
